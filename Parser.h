6d0fdb050a14b97b3c6b94f00d79655b4851216a
neoner987 <128582371+neoner987@users.noreply.github.com>
2026-08-12 01:52:53 +0300

    Add parser header

---
+++ Parser.h
@@ -0,0 +1,77 @@
+#pragma once
+
+
+
+namespace Node {
+
+    struct NodeExpr {
+        Token m_int_lit;
+    };
+
+    struct NodeExit {
+        NodeExpr m_expr;
+    };
+
+}
+
+
+class Parser {
+
+public:
+    inline explicit Parser(std::vector<Token> tokens)
+        : m_tokens(std::move(tokens)){
+
+    }
+
+
+
+    std::optional<Node::NodeExpr> parse_expr() {
+        if (peak().has_value() && peak().value().Type == TokenType::int_lit) {
+            return Node::NodeExpr {
+                .m_int_lit = consume()
+            };
+        }
+    }
+
+    std::optional<Node::NodeExit> parse() {
+        std::optional<Node::NodeExit> exit_node;
+        while (peak().has_value()) {
+            if (peak().value().Type == TokenType::exit) {
+                consume();
+                if (auto node_expr = parse_expr()) {
+                    exit_node = Node::NodeExit { .m_expr = node_expr.value() };
+                }
+                else {
+                    std::cout << "Bruh! Invalid expr after exit.";
+                    exit(EXIT_FAILURE);
+                }
+                if (!peak().has_value() || peak().value().Type != TokenType::semi) {
+                    std::cout << "Bruh! No semi after exit T_T ";
+                    exit(EXIT_FAILURE);
+                }
+                else {
+                    consume();
+                }
+            }
+        }
+        m_index = 0;
+        return exit_node;
+    }
+
+
+private:
+    [[nodiscard]] inline std::optional<Token> peak(int offset = 0) const {
+        if (m_index + offset >= m_tokens.size()) {
+            return {};
+        } else {
+            return m_tokens[m_index + offset];
+        }
+    };
+
+    inline Token consume() {
+        return m_tokens[m_index++];
+    }
+    size_t m_index = 0;
+    const std::vector<Token> m_tokens;
+};
+
