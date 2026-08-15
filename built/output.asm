db851e44d90142dd2fe5bd4e41c5cd97925e62ca
neoner987 <128582371+neoner987@users.noreply.github.com>
2026-08-16 00:46:37 +0300

    just testing

--- built/output.asm
+++ built/output.asm
@@ -1,14 +1,24 @@
 global _start
 _start:
+	mov rax, 4
+	push rax
 	mov rax, 5
 	push rax
-	mov rax, 8
+	mov rax, 10
 	push rax
-	push QWORD [rsp + 8]
+	push QWORD [rsp + 16]
+
+	push QWORD [rsp + 16]
+
+	push QWORD [rsp + 16]
 
 	pop rax
 	pop rbx
 	imul rax, rbx
+	push rax
+	pop rax
+	pop rbx
+	add rax, rbx
 	push rax
 	push QWORD [rsp + 0]
 
